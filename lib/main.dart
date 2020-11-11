4e278b3987b6c2ca2c8867811b6f0623af1ad1ac
Gabriel Rascovit <rascovit@gmail.com>
2020-11-10 23:50:01 -0300

    add firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:lembrete/screens/home.dart';
 import 'package:lembrete/screens/reminder_form.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(App());
 }
 
